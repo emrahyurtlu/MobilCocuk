8db8e1fd633477ed28a7f09b3284f43c57ca2cef
Emrah Yurtlu <emrahyurtlu@gmail.com>
2020-09-01 00:35:58 +0300

    Ekran parlaklığı ve ses ayarları değişiyor.

--- app/src/main/java/com/mobilcocuk/DeviceDetailActivity.java
+++ app/src/main/java/com/mobilcocuk/DeviceDetailActivity.java
@@ -1,14 +1,88 @@
 package com.mobilcocuk;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.SeekBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class DeviceDetailActivity extends AppCompatActivity {
+    public AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_detail);
+
+        audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+
+        SeekBar screenBrightness = findViewById(R.id.screenBrightness);
+        screenBrightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (Settings.System.canWrite(getApplicationContext())) {
+
+                    Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, progress);
+
+
+                } else
+                    getSystemWriteRequest();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        SeekBar soundVolume = findViewById(R.id.soundVolume);
+        soundVolume.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+        soundVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, AudioManager.FLAG_PLAY_SOUND);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        try {
+            int b = Settings.System.getInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS);
+            screenBrightness.setProgress(b);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void getSystemWriteRequest() {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+        intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+        startActivity(intent);
+    }
+
+    public void goToQrGenerator(View view) {
+        Intent intent = new Intent(this, QrGeneratorActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/mobilcocuk/SignInActivity.java
+++ app/src/main/java/com/mobilcocuk/SignInActivity.java
@@ -2,7 +2,9 @@ package com.mobilcocuk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void goToMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
